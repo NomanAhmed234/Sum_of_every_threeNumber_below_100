158ad9c7d49829407e156251257d59bc004d85a1
Noman Ahmed <nomanpb234@gmail.com>
2023-07-18 01:12:19 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab_3d
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2, sum=2;
+            do
+            {
+                sum = sum + 3;
+                i++;
+            } while (i < 100);
+            Console.WriteLine("Sum of every third integer beginning from 1 to 100 is {0}.",sum);
+
+        }
+    }
+}
